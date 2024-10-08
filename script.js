05e614c87fd5016cc39642f7827c22b57a37b3a2
antonio <antoniobiskupec1@gmail.com>
2024-10-08 22:48:47 +0200

    first commit, made navbar and 2 sections

---
+++ script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const container = document.querySelector(".container-two");
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          container.classList.add("animate");
+          observer.unobserve(container); // Stop observing once animation is triggered
+        }
+      });
+    },
+    { threshold: 0.1 }
+  );
+
+  observer.observe(container);
+});
