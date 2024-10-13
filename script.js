a625e9f558fb46f136a3d41f30839c6ce8fbb4a4
antonio <antoniobiskupec1@gmail.com>
2024-10-13 12:23:59 +0200

    added mansory layout, cjenik section that needs to be finished and contact page that needs to be finished

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 // Observer for container-two
 
 document.addEventListener("DOMContentLoaded", function () {
-  const container = document.querySelector(".container-two");
+  const containerTwo = document.querySelector(".container-two");
 
   const observer = new IntersectionObserver(
     (entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
-          container.classList.add("animate");
-          observer.unobserve(container); // Stop observing once animation is triggered
+          containerTwo.classList.add("animate");
+          observer.unobserve(containerTwo); // Stop observing once animation is triggered
         }
       });
     },
     { threshold: 0.1 }
   );
 
-  observer.observe(container);
+  observer.observe(containerTwo);
 });
