d477824fc43e97654e85577830b8955fe413a909
antonio <antoniobiskupec1@gmail.com>
2024-10-24 20:33:13 +0200

    changed navbar and added hamburger menu, now we just need to fix first container

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+// Hamburger menu in navbar
+const primaryNav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click", () => {
+  const visibility = primaryNav.getAttribute("data-visible");
+
+  if (visibility === "false") {
+    primaryNav.setAttribute("data-visible", true);
+    navToggle.setAttribute("aria-expanded", true);
+  } else if (visibility === "true") {
+    primaryNav.setAttribute("data-visible", false);
+    navToggle.setAttribute("aria-expanded", false);
+  }
+});
 // Observer for container-two
 
 document.addEventListener("DOMContentLoaded", function () {
